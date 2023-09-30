5f3511d63801426fa1143c34ce18a5e26c679aca
diksonnn <146518361+diksonnn@users.noreply.github.com>
2023-09-30 12:25:28 +0500

    Add files via upload

---
+++ lab01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    internal class Program
+    {
+        static public int Maximum(int a, int b)
+        {
+            if (a > b)
+                return a;
+            return b;
+        }
+
+        static public int Minimum(int a, int b)
+        {
+            if (a < b)
+                return a;
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine((a+b)/2);
+            Console.WriteLine(Maximum(Math.Abs(a),Math.Abs(b)) - Minimum(Math.Abs(a), Math.Abs(b)));
+        }
+    }
+}
